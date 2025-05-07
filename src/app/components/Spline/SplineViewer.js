161da86bb1dd3b8bb8695846054c171a559b5389
Sandeep Bhaskar <ssandeepachari1@gmail.com>
2025-05-07 14:45:39 +0530

    mobile view

--- src/app/components/Spline/SplineViewer.js
+++ src/app/components/Spline/SplineViewer.js
@@ -1,14 +1,13 @@
-'use client';
+'use client'
 
 import dynamic from 'next/dynamic';
-import './SplineViewer.css';
-
-const Spline = dynamic(() => import('@splinetool/react-spline'), { ssr: false });
-
+import React from 'react';
+import Spline from '@splinetool/react-spline';
+import './SplineViewer.css'
 
 export default function SplineViewer() {
   return (
-    <div className="background">
+    <div className='background'>
       <Spline scene="https://prod.spline.design/VzxNs53BPBy2CW3p/scene.splinecode" />
     </div>
   );
