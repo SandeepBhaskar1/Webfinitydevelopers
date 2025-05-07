0cf526fc75b3a34b6de1aac6c56616d790c8ac67
Sandeep Bhaskar <ssandeepachari1@gmail.com>
2025-05-07 14:42:30 +0530

    ffc

--- src/app/components/Spline/SplineViewer.js
+++ src/app/components/Spline/SplineViewer.js
@@ -1,20 +1,16 @@
 'use client';
 
-import React, { Suspense } from 'react';
 import dynamic from 'next/dynamic';
 import './SplineViewer.css';
 
-const SplineComponent = dynamic(() => import('@splinetool/react-spline'), {
+const Spline = dynamic(() => import('@splinetool/react-spline/next'), {
   ssr: false,
-  loading: () => <div className="loading-container">Loading Spline Scene...</div>, // Add a loading indicator
 });
 
 export default function SplineViewer() {
   return (
     <div className="background">
-      <Suspense fallback={<div className="loading-container">Loading Spline Scene...</div>}>
-        <SplineComponent scene="https://prod.spline.design/VzxNs53BPBy2CW3p/scene.splinecode" />
-      </Suspense>
+      <Spline scene="https://prod.spline.design/VzxNs53BPBy2CW3p/scene.splinecode" />
     </div>
   );
 }
