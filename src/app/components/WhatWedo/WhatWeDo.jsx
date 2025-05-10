d38fd16a9b1747ce595ad236f2847db3b0959a43
Sandeep Bhaskar <ssandeepachari1@gmail.com>
2025-05-10 17:27:03 +0530

    service

--- src/app/components/WhatWedo/WhatWeDo.jsx
+++ src/app/components/WhatWedo/WhatWeDo.jsx
@@ -127,7 +127,10 @@ export default function Frame() {
       <div className="wrapper">
         {mobileView ? (
           <div className='mobile-view'>
-            <img className='mobile-service-01' src="/whatwedo/Services.svg" alt="Services" />
+            <div className="div">
+            <img className='mobile-service-01' onContextMenu={(e).preventDefault()} draggable='false' src="/whatwedo/Services.svg" alt="Services" />
+            <h2>Services</h2>
+            </div>
             <img className='mobile-globe' src="/whatwedo/Globe.svg" alt="Globe" />
             <img className='mobile-service-02' src="/whatwedo/Services.svg" alt="Service" />
             <img className='mobile-card' src="/whatwedo/Website-designing.svg" alt="Web Design" />
@@ -138,26 +141,54 @@ export default function Frame() {
         ) : (
           <>
             <div className="left">
-              <img className='new-01' src="/whatwedo/new-01.svg" alt="Left Graphic" />
+              <img className='new-01' src="/whatwedo/new-01.svg" alt="Left Graphic" onContextMenu={(e) => e.preventDefault()}/>
+              <h1>SOCIAL MEDIA MARKETING</h1>
+              <p>We create engaging content and run targeted ads to grow your brand, reach more customers, and drive real results across platforms like Instagram, Facebook, and Whatsapp.</p>
             </div>
 
             <div className="container">
               <div className="centre-top-part">
-                <img className='web-design' src="/whatwedo/Website-designing.svg" alt="Web Design" />
-                <img className='services' src="/whatwedo/Services.svg" alt="Services" />
-                <img className='e-commerce' src="/whatwedo/E-commerce.svg" alt="E-commerce" />
+                <div className="web-content">
+                <img className='web-design' src="/whatwedo/Website-designing.svg" alt="Web Design" onContextMenu={(e) => e.preventDefault()} draggable='false'/>
+                <h2>WEBSITE DESIGNING</h2>
+                <p>We create high quality professional customized responsive website for the best price.</p>
+                </div>
+                <div className="service-cont">
+                <img className='services' src="/whatwedo/Services.svg" alt="Services" onContextMenu={(e) => e.preventDefault()} draggable='false' />
+                <h1>SERVICES <br />WE OFFER</h1>
+                </div>
+
+                <div className="e-commerce-content">
+                <img className='e-commerce' src="/whatwedo/E-commerce.svg" alt="E-commerce" onContextMenu={(e) => e.preventDefault()} draggable='false' />
+                <h2>E-COMMERCE</h2>
+                <p>Sell your products online to your consumers using a professionally built website by us.</p>
+                </div>
               </div>
               <div className="centre">
-                <img src="/whatwedo/Globe.svg" alt="Globe" />
+                <img src="/whatwedo/Globe.svg" alt="Globe" onContextMenu={(e) => e.preventDefault()} draggable='false'/>
               </div>
               <div className="centre-bottom-part">
-                <img className='lead-generation' src="/whatwedo/Lead-Generation.svg" alt="Lead Generation" />
-                <img className='performance-marketing' src="/whatwedo/Performance-Marketing.svg" alt="Performance Marketing" />
+                <div className="lead">
+                <img className='lead-generation' src="/whatwedo/Lead-Generation.svg" alt="Lead Generation" onContextMenu={(e) => e.preventDefault()} draggable='false'/>
+                <h1>LEAD GENERATION</h1>
+                <p>Fuel your growth with our tailored strategies. Convert your audience into valuable leads for sustainable success.</p>
+                </div>
+
+                <div className="performance-content">
+                <img className='performance-marketing' src="/whatwedo/Performance-Marketing.svg" alt="Performance Marketing" onContextMenu={(e) => e.preventDefault()} draggable='false'/>
+                <h1>PERFORMANCE MARKETING</h1>
+                <p>To ensure higher ROI (returns on your investments), we use different strategies that are based on real-time research.</p>
+                </div>
               </div>
             </div>
 
             <div className="right">
-              <img src="/whatwedo/new-02.svg" alt="Right Graphic" />
+              <img src="/whatwedo/new-02.svg" alt="Right Graphic" onContextMenu={(e) => e.preventDefault()} draggable='false'/>
+              <img className="pencil-lady" src="/whatwedo/lady-with-pencil.svg" alt="" onContextMenu={(e) => e.preventDefault()} draggable='false'/>
+              <div className="branding-text">
+                <h1>BRANDING</h1>
+                <p>We build strong, memorable brands with the right mix of design, messaging, and strategy—helping you stand out and connect with your audience.</p>
+              </div>
             </div>
           </>
         )}
